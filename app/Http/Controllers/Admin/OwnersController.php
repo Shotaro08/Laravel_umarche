aac28738bd883fdcd744def3dccae92e2ad06e4a
shotaro08 <s.tr08.sbyi0915@gmail.com>
2022-03-31 14:20:48 +0900

    edit, update

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -8,6 +8,8 @@ use App\Models\Owner; // Eloquent
 use Illuminate\Support\Facades\DB; // querybuilder
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Router;
+use Illuminate\Routing\Redirector;
 
 class OwnersController extends Controller
 {
@@ -34,7 +36,7 @@ class OwnersController extends Controller
         // $e_all = Owner::all();
         // $q_get = DB::table('owners')->select('name', 'created_at')->get();
 
-        $owners = Owner::select('name', 'email', 'created_at')->get();
+        $owners = Owner::select('id', 'name', 'email', 'created_at')->get();
 
         return view('admin.owners.index', compact('owners'));
 
@@ -108,7 +110,9 @@ class OwnersController extends Controller
      */
     public function edit($id)
     {
-        //
+        $owner = Owner::findOrFail($id);
+
+        return view('admin.owners.edit', compact('owner'));
     }
 
     /**
@@ -120,7 +124,14 @@ class OwnersController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $owner = Owner::findOrFail($id);
+        $owner->name = $request->name;
+        $owner->email = $request->email;
+        $owner->password = Hash::make($request->password);
+
+        $owner->save();
+
+        return redirect()->route('admin.owners.index')->with('message', 'オーナー情報を更新しました');
     }
 
     /**
--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -22,6 +22,7 @@
                             <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">Name</th>
                             <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">email</th>
                             <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">created</th>
+                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100"></th>
                           </tr>
                         </thead>
                         <tbody>
@@ -30,6 +31,9 @@
                                 <td class="px-4 py-3 ">{{ $owner->name }}</td>
                                 <td class="px-4 py-3 ">{{ $owner->email }}</td>
                                 <td class="px-4 py-3 ">{{ $owner->created_at->diffForHumans() }}</td>
+                                <td class="px-4 py-3">
+                                  <button onclick="location.href='{{ route('admin.owners.edit', ['owner'  => $owner->id] )}}'"class="text-white bg-purple-400 border-0 py-2 px-4 focus:outline-none hover:bg-purple-500 rounded">編集</button>
+                                </td>
                               </tr>
                             @endforeach
                         </tbody>
