a74ac93640592390babbc7ac490dbf92090d1ca3
shotaro08 <s.tr08.sbyi0915@gmail.com>
2022-03-31 16:47:01 +0900

    pagination

--- database/seeders/OwnerSeeder.php
+++ database/seeders/OwnerSeeder.php
@@ -19,19 +19,44 @@ class OwnerSeeder extends Seeder
             [
                 'name' => 'test1',
                 'email' => 'test1@gmail.com',
-                'password' => Hash::make('passtest1'),
+                'password' => Hash::make('pass1111'),
                 'created_at' => '2022/03/29 13:28:00',
                 ],
             [
                 'name' => 'test2',
                 'email' => 'test2@gmail.com',
-                'password' => Hash::make('passtest2'),
+                'password' => Hash::make('pass2222'),
                 'created_at' => '2022/03/29 13:28:00',
             ],
             [
                 'name' => 'test3',
                 'email' => 'test3@gmail.com',
-                'password' => Hash::make('passtest3'),
+                'password' => Hash::make('pass3333'),
+                'created_at' => '2022/03/29 13:28:00',
+            ],
+            [
+                'name' => 'test4',
+                'email' => 'test4@gmail.com',
+                'password' => Hash::make('pass4444'),
+                'created_at' => '2022/03/29 13:28:00',
+            ],
+            [
+                'name' => 'test5',
+                'email' => 'test5@gmail.com',
+                'password' => Hash::make('pass5555'),
+                'created_at' => '2022/03/29 13:28:00',
+            ],
+
+            [
+                'name' => 'test6',
+                'email' => 'test6@gmail.com',
+                'password' => Hash::make('pass6666'),
+                'created_at' => '2022/03/29 13:28:00',
+            ],
+            [
+                'name' => 'test7',
+                'email' => 'test7@gmail.com',
+                'password' => Hash::make('pass7777'),
                 'created_at' => '2022/03/29 13:28:00',
             ],
         ]);
