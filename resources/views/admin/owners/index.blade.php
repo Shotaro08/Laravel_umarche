90a9d59693490297b1e7bbc05dae1416743224c4
shotaro08 <s.tr08.sbyi0915@gmail.com>
2022-03-31 11:02:14 +0900

    index, create

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
   <x-slot name="header">
       <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-          {{ __('Dashboard') }}
+          オーナー一覧
       </h2>
   </x-slot>
 
@@ -9,20 +9,37 @@
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
               <div class="p-6 bg-white border-b border-gray-200">
-                  eloquent <br>
-                  @foreach ($e_all as $e_owner )
-                    {{ $e_owner->name }}
-                    {{ $e_owner->created_at->diffForHumans() }}
-                    <br>
-                  @endforeach
-                  <br>
-                  <br>
-                  querybuilder <br>
-                  @foreach ($q_get as $q_owner)
-                    {{ $q_owner->name }}
-                    {{ Carbon\Carbon::parse($q_owner->created_at)->diffForHumans() }}
-                    <br>
-                  @endforeach
+                <section class="text-gray-600 body-font">
+                  <div class="container mx-auto">
+                    <div class="flex justify-end mb-4">
+                      <button onclick="location.href='{{ route('admin.owners.create') }}'"class="text-white bg-purple-500 border-0 py-2 px-8 focus:outline-none hover:bg-purple-600 rounded text-lg">新規登録</button>
+                    </div>
+                    <div class="lg:w-2/3 w-full mx-auto overflow-auto">
+                      <table class="table-auto w-full text-left whitespace-no-wrap">
+                        <thead>
+                          <tr>
+                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">Name</th>
+                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">email</th>
+                            <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">created</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          @foreach ($owners as $owner)
+                            <tr>
+                                <td class="px-4 py-3 ">{{ $owner->name }}</td>
+                                <td class="px-4 py-3 ">{{ $owner->email }}</td>
+                                <td class="px-4 py-3 ">{{ $owner->created_at->diffForHumans() }}</td>
+                              </tr>
+                            @endforeach
+                        </tbody>
+                      </table>
+                    </div>
+                    <div class="flex pl-4 mt-4 lg:w-2/3 w-full mx-auto">
+                    </div>
+                  </div>
+                </section>
+
+
               </div>
           </div>
       </div>
