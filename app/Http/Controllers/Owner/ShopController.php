d33193613f039fc63a1cd95aad7a9c518cc81d00
Shotaro08 <s.tr08.sbyi0915@gmail.com>
2022-04-03 15:13:32 +0900

    create shopindex

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -7,11 +7,31 @@ use App\Models\Shop;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+use function PHPUnit\Framework\isNull;
+
 class ShopController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth:owners');
+
+        $this->middleware(function ($request, $next) {
+
+            // dd($request->route()->parameter('shop')); //文字列
+            // dd(Auth::id());
+
+            $id = $request->route()->parameter('shop');
+            if(!is_null($id)){
+                $shopsId = Shop::findOrFail($id)->owner->id;
+                $shopId = (int)$shopsId;
+                $ownerId = Auth::id();
+                if($shopId !== $ownerId){
+                    abort(404);
+                }
+            }
+            
+            return $next($request);
+        });
     }
 
     public function index()
