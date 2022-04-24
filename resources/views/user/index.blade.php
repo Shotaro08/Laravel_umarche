07f22423a5cbbf35739340261ea78f49f7977eb4
Shotaro08 <s.tr08.sbyi0915@gmail.com>
2022-04-24 21:51:07 +0900

    description ready

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -12,9 +12,11 @@
                     <div class="flex flex-wrap ">
                         @foreach ($products as $product)
                             <div class="w-1/4 p-2 md:p-4 ">
-                                <div class="border rounded-md p-2 md:p-4">
-                                    <x-thumnail filename="{{ $product->filename ?? '' }}"
-                                        type="products" />
+                                <a href="{{ route('user.items.show', ['item' => $product->id]) }}">
+                                    <div class="border rounded-md p-2 md:p-4">
+                                        <x-thumnail filename="{{ $product->filename ?? '' }}"
+                                            type="products" />
+                                </a>
                                     <div class="mt-4">
                                         <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">{{ $product->category }}</h3>
                                         <h2 class="text-gray-900 title-font text-lg font-medium">{{ $product->name }}</h2>
