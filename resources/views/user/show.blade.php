03eda01da205d18241d928a0dfa80b2d9ee73fce
Shotaro08 <s.tr08.sbyi0915@gmail.com>
2022-04-29 16:49:26 +0900

    cart add

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -73,21 +73,25 @@
                                         class="title-font font-medium text-2xl text-gray-900">{{ number_format($product->price) }}</span>
                                     <span class="text-sm text-gray-700">円（税込）</span>
                                 </div>
-                                <div class="flex items-center">
-                                    <span class="mr-3">数量</span>
-                                    <div class="relative">
-                                        <select name="quantity"
-                                            class="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-purple-200 focus:border-purple-500 text-base pl-3 pr-10">
-                                            @for ($i = 1; $i <= $quantity; $i++)
-                                            <option value="{{ $i }}" >{{ $i }}</option>
+                                <form action="{{ route('user.cart.add') }}" method="post">
+                                    @csrf
+                                    <div class="flex items-center">
+                                        <span class="mr-3">数量</span>
+                                        <div class="relative">
+                                            <select name="quantity"
+                                                class="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-purple-200 focus:border-purple-500 text-base pl-3 pr-10">
+                                                @for ($i = 1; $i <= $quantity; $i++)
+                                                <option value="{{ $i }}" >{{ $i }}</option>
 
-                                            @endfor
+                                                @endfor
 
-                                        </select>
+                                            </select>
+                                        </div>
                                     </div>
-                                </div>
-                                <button
-                                    class="flex ml-auto text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded">カートに入れる</button>
+                                    <button
+                                        class="flex ml-auto text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded">カートに入れる</button>
+                                    <input type="hidden" name="product_id" value="{{ $product->id }}">
+                                </form>
 
                             </div>
                         </div>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ComponentTestController;
 use App\Http\Controllers\LifeCycleTestController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User\itemController;
+use App\Http\Controllers\User\CartController;
 
 
 
@@ -28,6 +29,12 @@ Route::middleware('auth:users')
 ->group(function () {
     Route::get('/', [ItemController::class, 'index'])->name('items.index');
     Route::get('show/{item}', [ItemController::class, 'show'])->name('items.show');
+});
+
+Route::prefix('cart')
+->middleware('auth:users')
+->group(function () {
+    Route::post('add', [CartController::class, 'add'])->name('cart.add');
 });
 
 // Route::get('/dashboard', function () {
