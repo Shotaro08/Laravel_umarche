c55959c86235d368ace91868b29cdc0c03e1c7ca
Shotaro08 <s.tr08.sbyi0915@gmail.com>
2022-04-29 17:40:09 +0900

    cart index

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -6,9 +6,25 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Cart;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class CartController extends Controller
 {
+    public function index()
+    {
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+        $totalPrice = 0;
+
+        foreach($products as $product){
+            $totalPrice += $product->price * $product->pivot->quantity;
+        }
+
+
+        return view('user.cart', compact('products', 'totalPrice'));
+
+    }
+
     public function add(Request $request)
     {
         $itemInCart = Cart::where('product_id', $request->product_id)
@@ -27,6 +43,6 @@ class CartController extends Controller
             ]);
         }
 
-        dd('test');
+        return redirect()->route('user.cart.index');
     }
 }
