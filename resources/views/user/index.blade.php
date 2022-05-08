84ea3fee50029a44b7a8a17653cfd8d5d0754410
Shotaro08 <s.tr08.sbyi0915@gmail.com>
2022-05-08 11:20:47 +0900

    index orderby

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,8 +1,48 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            HOME
-        </h2>
+        <div class="flex justify-between itemscenter">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                商品一覧
+            </h2>
+            <div>
+                <form method="get" action="{{ route('user.items.index') }}">
+                    <div class="flex">
+                        <div>
+                            <span class="text-sm">表示順</span><br>
+                            <select name="sort" id="sort" class="mr-4" >
+                                <option value="{{ \App\Constants\Common::SORT_ORDER['recommend'] }}"
+                                    @if (Request::get('sort') === \App\Constants\Common::SORT_ORDER['recommend'])
+                                        selected
+                                    @endif>おすすめ順
+                                </option>
+                                <option value="{{ \App\Constants\Common::SORT_ORDER['higherPrice'] }}"
+                                    @if (Request::get('sort') === \App\Constants\Common::SORT_ORDER['higherPrice'])
+                                        selected
+                                    @endif>価格の高い順
+                                </option>
+                                <option value="{{ \App\Constants\Common::SORT_ORDER['lowerPrice'] }}"
+                                    @if (Request::get('sort') === \App\Constants\Common::SORT_ORDER['lowerPrice'])
+                                        selected
+                                    @endif>価格の低い順
+                                </option>
+                                <option value="{{ \App\Constants\Common::SORT_ORDER['later'] }}"
+                                    @if (Request::get('sort') === \App\Constants\Common::SORT_ORDER['later'])
+                                        selected
+                                    @endif>新しい順
+                                </option>
+                                <option value="{{ \App\Constants\Common::SORT_ORDER['older'] }}"
+                                    @if (Request::get('sort') === \App\Constants\Common::SORT_ORDER['older'])
+                                        selected
+                                    @endif>古い順
+                                </option>
+                            </select>
+                        </div>
+                        <div>表示件数</div>
+                    </div>
+                </form>
+
+            </div>
+        </div>
     </x-slot>
 
     <div class="py-12">
@@ -14,21 +54,28 @@
                             <div class="w-1/4 p-2 md:p-4 ">
                                 <a href="{{ route('user.items.show', ['item' => $product->id]) }}">
                                     <div class="border rounded-md p-2 md:p-4">
-                                        <x-thumnail filename="{{ $product->filename ?? '' }}"
-                                            type="products" />
+                                        <x-thumnail filename="{{ $product->filename ?? '' }}" type="products" />
                                 </a>
-                                    <div class="mt-4">
-                                        <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">{{ $product->category }}</h3>
-                                        <h2 class="text-gray-900 title-font text-lg font-medium">{{ $product->name }}</h2>
-                                        <p class="mt-1">{{ number_format($product->price) }} <span class="text-sm text-gray-700">円（税込）</span></p>
-                                    </div>
+                                <div class="mt-4">
+                                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
+                                        {{ $product->category }}</h3>
+                                    <h2 class="text-gray-900 title-font text-lg font-medium">{{ $product->name }}</h2>
+                                    <p class="mt-1">{{ number_format($product->price) }} <span
+                                            class="text-sm text-gray-700">円（税込）</span></p>
                                 </div>
-                                </a>
                             </div>
-                        @endforeach
+                            </a>
                     </div>
+                    @endforeach
                 </div>
             </div>
         </div>
     </div>
+    </div>
+    <script>
+        const select = document.getElementById('sort')
+        select.addEventListener('change', function(){
+            this.form.submit()
+        })
+    </script>
 </x-app-layout>
