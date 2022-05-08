23b16fd7ad6524144b8e58d130b09e69569d2f50
Shotaro08 <s.tr08.sbyi0915@gmail.com>
2022-05-08 09:49:16 +0900

    fix constractor

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -7,12 +7,29 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Stock;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class ItemController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth:users');
+
+        $this->middleware(function ($request, $next) {
+
+            // dd($request->route()->parameter('shop')); //文字列
+            // dd(Auth::id());
+
+            $id = $request->route()->parameter('item');
+            if (!is_null($id)) {
+                $itemId = Product::availableItems()->where('products.id', $id)->exists();
+                if (!$itemId) {
+                    abort(404);
+                }
+            }
+
+            return $next($request);
+        });
     }
 
     public function index(){
