030bac3fadda9b2fd4e9ace73a1d0183ef7093f4
Shotaro08 <s.tr08.sbyi0915@gmail.com>
2022-05-15 09:17:30 +0900

    検索フォーム カテゴリー

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,67 +1,62 @@
 <x-app-layout>
     <x-slot name="header">
-        <div class="flex justify-between itemscenter">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                商品一覧
-            </h2>
-            <div>
-                <form method="get" action="{{ route('user.items.index') }}">
-                    <div class="flex">
+
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            商品一覧
+        </h2>
+        <form method="get" action="{{ route('user.items.index') }}">
+            <div class="lg:flex lg:justify-around">
+                <div class="lg:flex items-center">
+                    <select name="category" class="mb-2 lg:mb-0 lg:mr-2">
+                        <option value="0" @if(\Request::get('category') === '0') selected @endif>すべて</option>
+                        @foreach ($categories as $category)
+                            <optgroup label="{{ $category->name }}"></optgroup>
+                            @foreach ($category->secondary as $secondary)
+                                <option value="{{ $secondary->id }}" @if(\Request::get('category') == $secondary->id ) selected @endif>
+                                    {{ $secondary->name }}
+                                </option>
+                            @endforeach
+                        @endforeach
+                    </select>
+                    <div class="flex space-x-2 items-center">
+                        <div><input name="keyword" class="border border-gray-500 py-2" placeholder="キーワードを入力"></div>
                         <div>
-                            <span class="text-sm">表示順</span><br>
-                            <select name="sort" id="sort" class="mr-4" >
-                                <option value="{{ \App\Constants\Common::SORT_ORDER['recommend'] }}"
-                                    @if (Request::get('sort') === \App\Constants\Common::SORT_ORDER['recommend'])
-                                        selected
-                                    @endif>おすすめ順
-                                </option>
-                                <option value="{{ \App\Constants\Common::SORT_ORDER['higherPrice'] }}"
-                                    @if (Request::get('sort') === \App\Constants\Common::SORT_ORDER['higherPrice'])
-                                        selected
-                                    @endif>価格の高い順
-                                </option>
-                                <option value="{{ \App\Constants\Common::SORT_ORDER['lowerPrice'] }}"
-                                    @if (Request::get('sort') === \App\Constants\Common::SORT_ORDER['lowerPrice'])
-                                        selected
-                                    @endif>価格の低い順
-                                </option>
-                                <option value="{{ \App\Constants\Common::SORT_ORDER['later'] }}"
-                                    @if (Request::get('sort') === \App\Constants\Common::SORT_ORDER['later'])
-                                        selected
-                                    @endif>新しい順
-                                </option>
-                                <option value="{{ \App\Constants\Common::SORT_ORDER['older'] }}"
-                                    @if (Request::get('sort') === \App\Constants\Common::SORT_ORDER['older'])
-                                        selected
-                                    @endif>古い順
-                                </option>
-                            </select>
-                        </div>
-                        <div>
-                            <span class="text-sm">表示件数</span><br>
-                            <select name="pagination" id="pagination">
-                                <option value="20"
-                                @if (\Request::get('pagination') === '20')
-                                selected
-                                @endif>20件
-                                </option>
-                                <option value="50"
-                                @if (\Request::get('pagination') === '50')
-                                selected
-                                @endif>50件
-                                </option>
-                                <option value="100"
-                                @if (\Request::get('pagination') === '100')
-                                selected
-                                @endif>100件
-                                </option>
-                            </select>
+                            <button
+                                class="ml-auto text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded">検索する</button>
                         </div>
                     </div>
-                </form>
-
+                </div>
+                <div class="flex">
+                    <div>
+                        <span class="text-sm">表示順</span><br>
+                        <select name="sort" id="sort" class="mr-4">
+                            <option value="{{ \App\Constants\Common::SORT_ORDER['recommend'] }}" @if (Request::get('sort') === \App\Constants\Common::SORT_ORDER['recommend']) selected @endif>おすすめ順
+                            </option>
+                            <option value="{{ \App\Constants\Common::SORT_ORDER['higherPrice'] }}" @if (Request::get('sort') === \App\Constants\Common::SORT_ORDER['higherPrice']) selected @endif>価格の高い順
+                            </option>
+                            <option value="{{ \App\Constants\Common::SORT_ORDER['lowerPrice'] }}" @if (Request::get('sort') === \App\Constants\Common::SORT_ORDER['lowerPrice']) selected @endif>価格の低い順
+                            </option>
+                            <option value="{{ \App\Constants\Common::SORT_ORDER['later'] }}" @if (Request::get('sort') === \App\Constants\Common::SORT_ORDER['later']) selected @endif>新しい順
+                            </option>
+                            <option value="{{ \App\Constants\Common::SORT_ORDER['older'] }}" @if (Request::get('sort') === \App\Constants\Common::SORT_ORDER['older']) selected @endif>古い順
+                            </option>
+                        </select>
+                    </div>
+                    <div>
+                        <span class="text-sm">表示件数</span><br>
+                        <select name="pagination" id="pagination">
+                            <option value="20" @if (\Request::get('pagination') === '20') selected @endif>20件
+                            </option>
+                            <option value="50" @if (\Request::get('pagination') === '50') selected @endif>50件
+                            </option>
+                            <option value="100" @if (\Request::get('pagination') === '100') selected @endif>100件
+                            </option>
+                        </select>
+                    </div>
+                </div>
             </div>
-        </div>
+        </form>
+
     </x-slot>
 
     <div class="py-12">
@@ -78,7 +73,8 @@
                                 <div class="mt-4">
                                     <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
                                         {{ $product->category }}</h3>
-                                    <h2 class="text-gray-900 title-font text-lg font-medium">{{ $product->name }}</h2>
+                                    <h2 class="text-gray-900 title-font text-lg font-medium">{{ $product->name }}
+                                    </h2>
                                     <p class="mt-1">{{ number_format($product->price) }} <span
                                             class="text-sm text-gray-700">円（税込）</span></p>
                                 </div>
@@ -88,22 +84,23 @@
                     @endforeach
                 </div>
                 {{ $products->appends([
-                    'sort' => \Request::get('sort'),
-                    'pagination' => \Request::get('pagination'),
-                ])->links() }}
+        'sort' => \Request::get('sort'),
+        'pagination' => \Request::get('pagination'),
+    ])->links() }}
             </div>
         </div>
     </div>
     </div>
     <script>
         const select = document.getElementById('sort')
-        select.addEventListener('change', function(){
+        select.addEventListener('change', function() {
             this.form.submit()
         })
 
         const paginate = document.getElementById('pagination')
-        paginate.addEventListener('change', function(){
+        paginate.addEventListener('change', function() {
             this.form.submit()
         })
+
     </script>
 </x-app-layout>
